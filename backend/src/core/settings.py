478de03ad5ed0ac8039fce0d5416786542e46dca
mauricio <mauriciobenjamin700@gmail.com>
2025-09-20 09:11:01 -0300

    config: queue configured

---
+++ backend/src/core/settings.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        str_strip_whitespace=True,
+        case_sensitive=True
+    )
+
+    # Application
+    APP_NAME: str = "FastStream Learning"
+    APP_VERSION: str = "0.1.0"
+
+    # Server
+    DB_URL: str = "sqlite+aiosqlite:///./test.db"
+    DB_ECHO: bool = False
+
+    BROKER_URL: str = "amqp://guest:guest@localhost:5672//"
+
